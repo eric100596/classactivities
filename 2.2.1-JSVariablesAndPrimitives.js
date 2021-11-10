b025bc19993891c124d6babecddf968060007ffc
Eric Baker <eric100596@aol.com>
2021-11-09 21:40:44 -0600

    Activities for section 2.2.1

---
+++ 2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,31 @@
+// create a constant variable (const)
+const bunny;
+
+// create a variable that can be reassigned (let)
+let wd40;
+wd40 = "house";
+// create three variables and assign them values of different data types (=)
+let g = "sleep"
+let c = 23;
+let joke = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+let c = 23;
+console.log(typeof c);
+
+
+// change the value referenced by a variable (dynamic typing)
+let variable1 = false;
+variable1 = 24;
+
+// print the type of the variable you just changed (typeof)
+
+
+// create variables and give them values to complete the sentences that will print
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+let name = "Tom";
+let color = "blue";
+let number = 5;
+let sunny = false;
+console.log("my name is " + name + ", my eyes are " + color + ". My favorite number " + number + ".")
