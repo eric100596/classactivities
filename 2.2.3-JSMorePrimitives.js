1ed4f7fe90d1931b35e6588cc4666e04c11f6810
Eric Baker <eric100596@aol.com>
2021-11-10 17:07:41 -0600

    JS More Primitives

---
+++ 2.2.3-JSMorePrimitives.js
@@ -0,0 +1,25 @@
+// create a variable "timeOfDay" and assign it a string
+let timeOfDay = "12:00";
+// create a variable "greeting" that references a template literal
+let greeting = `"Hello!" It's ${timeOfDay}"`;
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+
+// print "greeting"
+console.log(greeting);
+// create a new variable, but do not assign it a value
+let gas;
+// print the variable and its type
+// ? what type should we expect?
+gas is undefined
+// assign the variable a value that indicates the variable is purposely blank
+// ? what value should we assign?
+let gas =;
+// print the variable and its type again
+// ? what type should we expect?
+error msg
+// try to print a variable that does not exist
+// ? what should we expect to print in the CLI?
+
+// print "greeting" again
+// ? will this line run?
+not without the "let" statement;
