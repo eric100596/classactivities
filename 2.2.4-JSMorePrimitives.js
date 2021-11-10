76082097b6c0c211751844cab5e27b513749df30
Eric Baker <eric100596@aol.com>
2021-11-10 17:21:02 -0600

    JS More Primitives 2

---
+++ 2.2.4-JSMorePrimitives.js
@@ -0,0 +1,25 @@
+// create five variables and assign them values
+let a = {};
+let b =
+let gas = "price";
+let me = 4;
+let no = false;
+// each variable should reference a different primitive data type
+// ! REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+
+// create a variable that references a template literal
+// inside the template literal, use two variables
+let name = "Eric";
+let weather = "food";
+let practice = `"Hello ${name}," I said, "how's the ${weather}?"`;
+console.log(practice);
+// reassign the value of the variable that references "null"
+
+// print the value and its type
+
+// print a variable that causes a ReferenceError
+
+// alter the previous line to no longer cause a ReferenceError
