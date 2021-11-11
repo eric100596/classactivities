a3b6d094bd04af6607674ea55c28c5cdb0fe5681
Eric Baker <eric100596@aol.com>
2021-11-10 20:11:31 -0600

    Comparison Operators

---
+++ Section02/2.3.3-ComparisonOperators.js
@@ -0,0 +1,33 @@
+// ? log only the expressions that evaluate to true
+console.log(14 > 8);
+console.log(42 > 44);
+console.log(1 > 1);
+
+console.log(31 < 22);
+console.log(9 < 13);
+console.log(0 < 0);
+
+console.log(12 >= 12.5);
+console.log(60 >= 40);
+console.log(2 >= 2);
+
+console.log(12 <= 12.5);
+console.log(14 <= 0.3);
+console.log(5 <= 5);
+
+3 === 3;
+3 === "3";
+"3" === "3";
+3 === 4;
+
+2 !== "2";
+"2" !== "2";
+2 !== 1;
+
+"two" == 2;
+2 == "2";
+2 == 2;
+
+3 != "three";
+"3" != 3;
+3 != 3;
