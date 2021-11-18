5c8d555dd4635060c402e5a9bd5020f328378b61
Eric Baker <eric100596@aol.com>
2021-11-17 19:53:49 -0600

    Week 2 Homework

---
+++ Section02/Week2Homework.js
@@ -0,0 +1,13 @@
+let p = "pizzaPlace";
+let n = "numberOfToppings";
+console.log("E's Pizza");
+console.log("10 toppings");
+
+console.log(`"Come on down to E's ${p}! Specializing in the 10 toppings pizza!"`);
+n = 12;
+if (n <= 10) {
+  console.log("Quality, not quantity");
+}
+else {
+  console.log("A whole lot of pizza.");
+}
