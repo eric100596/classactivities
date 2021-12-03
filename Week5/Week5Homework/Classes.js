a6ba4dce7f940a49e12f8e11e3cf764a65e69edc
Eric Baker <eric100596@aol.com>
2021-12-03 12:13:25 -0800

    Week 5 Homework

---
+++ Week5/Week5Homework/Classes.js
@@ -0,0 +1,12 @@
+class Beaches {
+    constructor(name, city, waveHeight) {
+this.name = name;
+this.city = city;
+this.waveHeight = waveHeight;
+    }
+    thisBeach() {
+        return `${this.name} beach is in ${this.city}, and has waves ${this.waveHeight} high`;
+    }
+}
+
+const huntington = new Beaches("Huntington", "Huntington Beach", "5 feet");
