a8e225b7ad644ff622d4dcf5419ed6e00304c1f5
Eric Baker <eric100596@aol.com>
2021-12-04 12:54:04 -0800

    Week 5 Homework

--- Week5/Week5Homework/Classes.js
+++ Week5/Week5Homework/Classes.js
@@ -6,7 +6,10 @@ this.waveHeight = waveHeight;
     }
     thisBeach() {
         return `${this.name} beach is in ${this.city}, and has waves ${this.waveHeight} high`;
+
     }
+    
 }
 
 const huntington = new Beaches("Huntington", "Huntington Beach", "5 feet");
+console.table(huntington);
