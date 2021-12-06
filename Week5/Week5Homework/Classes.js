480ec65937029dd667d363392d5d55e9af966e44
Eric Baker <eric100596@aol.com>
2021-12-05 17:56:59 -0800

    Week 5 Homework

--- Week5/Week5Homework/Classes.js
+++ Week5/Week5Homework/Classes.js
@@ -5,11 +5,26 @@ this.city = city;
 this.waveHeight = waveHeight;
     }
     thisBeach() {
-        return `${this.name} beach is in ${this.city}, and has waves ${this.waveHeight} high`;
+        return `${this.name} beach is in ${this.city}, and has waves ${this.waveHeight} high!`;
 
     }
     
 }
 
 const huntington = new Beaches("Huntington", "Huntington Beach", "5 feet");
-console.log(huntington);
+huntington.thisBeach();
+
+class bestBeaches extends Beaches {
+    constructor(name, city, waveHeight, attendance, exclusivity) {
+        super(name, city, waveHeight);
+        this.attendance = attendance;
+        this.exclusivity = exclusivity;
+    }
+    thisIsTheBestBeach() {
+        return `${this.name} beach is in ${this.city}, and has waves ${this.waveHeight} high! 
+        Also, ${this.name} has a yearly attendance of ${this.attendance}, and is ${this.exclusivity}.`;
+    }
+}
+
+const malibu = new bestBeaches("Malibu", "Malibu, CA", "6 feet", "33,000", "very exclusive");
+malibu.thisIsTheBestBeach();
